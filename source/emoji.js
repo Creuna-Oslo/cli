569bfe7c03a68304838c40c11be21c0e5221e123
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-27 20:53:23 +0200

    Removes double ternary in emoji function

--- source/emoji.js
+++ source/emoji.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 
 module.exports = function(emoji, fallback = '') {
+  const emojiString = emoji ? `${emoji}  ` : '';
+
   return process.stdout.isTTY && process.platform === 'darwin'
-    ? emoji
-      ? `${emoji} `
-      : ''
-    : fallback;
+    ? emojiString
+    : `${fallback} `;
 };
