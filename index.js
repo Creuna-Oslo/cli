6f634c94ecd8c8f534474c0ff5e702393af06a26
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-08 23:41:29 +0200

    Adds CWD as fallback when .creunarc.json is missing

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 /* eslint-env node */
+const currentVersion = require('./source/get-this-version');
 const messages = require('./source/messages');
 const [command, arg1, arg2] = process.argv.slice(2);
 
+// The following two blocks are placed here in order to ensure fast shell output for 'creuna' and 'creuna -v'. (Node.js spends a small eternity importing and parsing the dependencies.)
 if (!command) {
   messages.help();
+}
+
+if (process.argv.includes('--version') || process.argv.includes('-v')) {
+  messages.version(currentVersion);
+  process.exit(0);
 }
 
 const appCreator = require('@creuna/create-react-app');
@@ -12,7 +19,6 @@ const path = require('path');
 const semver = require('semver');
 
 const configstore = require('./source/configstore');
-const currentVersion = require('./source/get-this-version');
 const fetchLatestVersion = require('./source/fetch-latest-version');
 const getConfig = require('./source/get-config');
 const getNewAppInput = require('./source/get-new-app-input');
@@ -21,64 +27,59 @@ const maybeWriteVSCodeTasks = require('./source/maybe-write-vscode-tasks');
 const runReactScript = require('./source/run-react-script');
 const supportedCommands = require('./source/supported-commands');
 
-if (process.argv.includes('--version') || process.argv.includes('-v')) {
-  messages.version(currentVersion);
-  process.exit(0);
-}
-
 fetchLatestVersion();
 
 let shouldExit = false;
 
 if (!command) {
   shouldExit = true;
-} else if (command === supportedCommands.new) {
-  // 'new' does not require .creunarc.json
-  const _messages = messages;
-  const projectPath = path.join(process.cwd(), arg1 || '');
-
-  appCreator
-    .canWriteFiles(projectPath)
-    .then(() => getNewAppInput())
-    .then(answers =>
-      appCreator.writeFiles(Object.assign({}, answers, { projectPath }))
-    )
-    .then(async ({ messages }) => {
-      await maybeWriteVSCodeTasks(projectPath);
-
-      _messages.emptyLine();
-      _messages.messageList(messages);
-      _messages.emptyLine();
-    })
-    .catch(messages.error);
 } else if (Object.values(supportedCommands).includes(command)) {
-  // All other commands require .creunarc.json to run
-  getConfig()
-    .then(({ componentsPath, eslintConfig, mockupPath }) => {
-      if (command === supportedCommands.lib) {
-        return lib(componentsPath);
-      }
+  (async () => {
+    const {
+      componentsPath = process.cwd(),
+      eslintConfig,
+      mockupPath = process.cwd()
+    } = await getConfig();
 
-      const _messages = messages;
-      return runReactScript({
-        arg1,
-        arg2,
-        eslintConfig,
-        command,
-        componentsPath,
-        mockupPath
-      })
-        .then(({ messages }) => {
-          _messages.emptyLine();
-          _messages.messageList(messages);
-          _messages.emptyLine();
+    if (command === supportedCommands.lib) {
+      return lib(componentsPath);
+    }
+
+    if (command === supportedCommands.new) {
+      const projectPath = path.join(process.cwd(), arg1 || '');
+
+      return appCreator
+        .canWriteFiles(projectPath)
+        .then(() => getNewAppInput())
+        .then(answers =>
+          appCreator.writeFiles(Object.assign({}, answers, { projectPath }))
+        )
+        .then(async response => {
+          await maybeWriteVSCodeTasks(projectPath);
+
+          messages.emptyLine();
+          messages.messageList(response.messages);
+          messages.emptyLine();
         })
         .catch(messages.error);
+    }
+
+    // If the command isn't 'new' or 'lib', the command is a @creuna/react-scripts command.
+    runReactScript({
+      arg1,
+      arg2,
+      eslintConfig,
+      command,
+      componentsPath,
+      mockupPath
     })
-    .catch(() => {
-      messages.errorReadingConfig();
-      shouldExit = true;
-    });
+      .then(response => {
+        messages.emptyLine();
+        messages.messageList(response.messages);
+        messages.emptyLine();
+      })
+      .catch(messages.error);
+  })();
 } else {
   messages.unrecognizedCommand(command);
   messages.help();
