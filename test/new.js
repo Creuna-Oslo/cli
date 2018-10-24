0fdf5d1e9c293051ed5059df927bb5c591593133
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-10-24 21:18:48 +0200

    Converts new app tests to not use snapshots

--- test/new.js
+++ test/new.js
@@ -7,9 +7,7 @@ const mockPrompt = require('./utils/mock-prompt');
 const mockMessages = require('./utils/mock-messages');
 const walkDirSync = require('./utils/walk-dir-sync');
 
-const template = async (t, answers) => {
-  t.plan(1);
-
+const template = async (t, answers, expectedFiles) => {
   const newApp = proxyquire('../source/new-app', {
     '@creuna/prompt': mockPrompt.bind(null, answers),
     './messages': mockMessages
@@ -20,28 +18,132 @@ const template = async (t, answers) => {
   await newApp(buildPath);
   const allFiles = walkDirSync(buildPath);
 
-  t.snapshot(
-    allFiles.map(filePath => filePath.replace(new RegExp(`${path.sep}/g`), '/'))
+  t.deepEqual(
+    expectedFiles.join('\n'),
+    allFiles
+      .map(filePath => filePath.replace(new RegExp(`${path.sep}/g`), '/'))
+      .join('\n')
   );
 };
 
-test('All options enabled', template, {
-  projectName: 'project',
-  authorName: 'author',
-  authorEmail: 'email',
-  useApiHelper: true,
-  useMessenger: true,
-  useAnalyticsHelper: true,
-  useResponsiveImages: true,
-  shouldWriteVSCodeTasks: true
-});
-test('All options disabled', template, {
-  projectName: 'project',
-  authorName: 'author',
-  authorEmail: 'email',
-  useApiHelper: false,
-  useMessenger: false,
-  useAnalyticsHelper: false,
-  useResponsiveImages: false,
-  shouldWriteVSCodeTasks: false
-});
+test(
+  'All options enabled',
+  template,
+  {
+    projectName: 'project',
+    authorName: 'author',
+    authorEmail: 'email',
+    useApiHelper: true,
+    useMessenger: true,
+    useAnalyticsHelper: true,
+    useResponsiveImages: true,
+    shouldWriteVSCodeTasks: true
+  },
+  [
+    '.babelrc',
+    '.creunarc.json',
+    '.editorconfig',
+    '.eslintignore',
+    '.eslintrc.json',
+    '.gitignore',
+    '.prettierignore',
+    '.vscode/tasks.json',
+    'README.md',
+    'browserslist',
+    'jsconfig.json',
+    'package.json',
+    'scripts/create-app-components.js',
+    'scripts/create-mockup-pages.js',
+    'scripts/prettier-config.js',
+    'source/components/README.md',
+    'source/components/fluid-image/fluid-image.jsx',
+    'source/components/fluid-image/fluid-image.scss',
+    'source/components/fluid-image/index.js',
+    'source/components/image/image.jsx',
+    'source/components/image/image.scss',
+    'source/components/image/index.js',
+    'source/components/message/index.js',
+    'source/components/message/message.jsx',
+    'source/components/message/message.scss',
+    'source/js/analytics.js',
+    'source/js/api-helper.js',
+    'source/js/input-detection-loader.js',
+    'source/js/input-detection.js',
+    'source/js/messenger.js',
+    'source/js/polyfills/class-list.js',
+    'source/js/responsive-images.js',
+    'source/js/server-polyfills.js',
+    'source/mockup/api/example-api-response.json',
+    'source/mockup/app.jsx',
+    'source/mockup/assets/uni.gif',
+    'source/mockup/layout.jsx',
+    'source/mockup/pages/example-page/example-page.json',
+    'source/mockup/pages/example-page/example-page.jsx',
+    'source/mockup/pages/example-page/index.js',
+    'source/mockup/pages/home.jsx',
+    'source/mockup/routes.jsx',
+    'source/scss/fonts.scss',
+    'source/scss/mixins.scss',
+    'source/scss/site.scss',
+    'source/scss/style.scss',
+    'source/scss/vars.scss',
+    'source/static.js',
+    'tests/.eslintrc.json',
+    'tests/example-page.js',
+    'webpack.config.js'
+  ]
+);
+
+test(
+  'All options disabled',
+  template,
+  {
+    projectName: 'project',
+    authorName: 'author',
+    authorEmail: 'email',
+    useApiHelper: false,
+    useMessenger: false,
+    useAnalyticsHelper: false,
+    useResponsiveImages: false,
+    shouldWriteVSCodeTasks: false
+  },
+  [
+    '.babelrc',
+    '.creunarc.json',
+    '.editorconfig',
+    '.eslintignore',
+    '.eslintrc.json',
+    '.gitignore',
+    '.prettierignore',
+    'README.md',
+    'browserslist',
+    'jsconfig.json',
+    'package.json',
+    'scripts/create-app-components.js',
+    'scripts/create-mockup-pages.js',
+    'scripts/prettier-config.js',
+    'source/components/README.md',
+    'source/js/input-detection-loader.js',
+    'source/js/input-detection.js',
+    'source/js/polyfills/class-list.js',
+    'source/js/server-polyfills.js',
+    'source/mockup/api/example-api-response.json',
+    'source/mockup/app.jsx',
+    'source/mockup/assets/uni.gif',
+    'source/mockup/layout.jsx',
+    'source/mockup/pages/example-page/example-page.json',
+    'source/mockup/pages/example-page/example-page.jsx',
+    'source/mockup/pages/example-page/index.js',
+    'source/mockup/pages/home.jsx',
+    'source/mockup/routes.jsx',
+    'source/scss/fonts.scss',
+    'source/scss/mixins.scss',
+    'source/scss/site.scss',
+    'source/scss/style.scss',
+    'source/scss/vars.scss',
+    'source/static.js',
+    'tests/.eslintrc.json',
+    'tests/example-page.js',
+    'webpack.config.js'
+  ]
+);
