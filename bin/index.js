1df5e3f12e2f07ed00fcc929fe36184a316df6e0
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-10-31 00:27:26 +0100

    Moves comment

--- bin/index.js
+++ bin/index.js
@@ -5,13 +5,13 @@ const currentVersion = require('../source/get-this-version');
 // eslint-disable-next-line no-unused-vars
 const [nodeJsPath, binPath, command, ...shellArguments] = process.argv;
 
+// The following two blocks are placed here in order to ensure fast shell output for 'creuna' and 'creuna -v'. (Node.js spends a small eternity importing and parsing the dependencies in ../index.js).
 if (process.argv.includes('--version') || process.argv.includes('-v')) {
   // eslint-disable-next-line no-console
   console.log(currentVersion);
   process.exit(0);
 }
 
-// The following two blocks are placed here in order to ensure fast shell output for 'creuna' and 'creuna -v'. (Node.js spends a small eternity importing and parsing the dependencies in ../index.js).
 if (!command) {
   const messages = require('../source/messages');
   messages.help();
